126e8ae7b9d38b67672d3a268ee9274064c7b5aa
Eduardo <gerson.perez@tismart.com>
2021-10-26 18:02:31 -0500

    avances

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function openMenuMobile() {
+  document.querySelector(".menu-content").style.width = "100%";
+}
+
+function closeMenuMobile() {
+  document.querySelector(".menu-content").style.width = "0";
+};
